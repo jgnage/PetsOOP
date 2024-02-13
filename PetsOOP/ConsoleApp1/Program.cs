9087230707a8c1bc506f078ac84de8d7efb5b56c
jgnage <jlg243@pitt.edu>
2024-02-13 02:32:43 -0500

    Program.cs Update
    Contains an update to have two pets meet and list all available pets.

--- PetsOOP/ConsoleApp1/Program.cs
+++ PetsOOP/ConsoleApp1/Program.cs
@@ -1,9 +1,8 @@
-﻿
-
-
-using ClassLibrary1;
+﻿using ClassLibrary1;
 
 string input = "";
+
+List<Pet> pets = new List<Pet>();
 
 while (input != "4")
 {
@@ -25,20 +24,73 @@ while (input != "4")
             switch (input2)
             {
                 case "1":
-                    Cat newCat = new Cat(petName);
+                    pets.Add(new Cat(petName)); 
                     break;
                 case "2":
-                    Chimpanzee newChimp = new Chimpanzee(petName);
+                    pets.Add(new Chimpanzee(petName)); 
                     break;
                 case "3":
-                    Turtle newTurtle = new Turtle(petName);
+                    pets.Add(new Turtle(petName)); 
                     break;
             }
-
             break;
         case "2":
+            Console.WriteLine("All of your pets:");
+            if (pets.Count == 0)
+            {
+                Console.WriteLine("No pets available.");
+            }
+            else
+            {
+                foreach (Pet pet in pets)
+                {
+                    Console.WriteLine($"Name: {pet.Name}, Type: {pet.GetType().Name}");
+                }
+            }
             break;
         case "3":
+            if (pets.Count < 2)
+            {
+                Console.WriteLine("You need two pets for them to meet.");
+            }
+            else
+            {
+                Console.WriteLine("Select the first pet:");
+                for (int i = 0; i < pets.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {pets[i].Name}");
+                }
+                Console.Write("Enter the number of the first pet: ");
+                int petIndex1 = int.Parse(Console.ReadLine()) - 1;
+
+                Console.WriteLine("Select the second pet:");
+                for (int i = 0; i < pets.Count; i++)
+                {
+                    if (i != petIndex1)
+                    {
+                        Console.WriteLine($"{i + 1}. {pets[i].Name}");
+                    }
+                }
+                Console.Write("Enter the number of the second pet: ");
+                int petIndex2 = int.Parse(Console.ReadLine()) - 1;
+
+                Pet pet1 = pets[petIndex1];
+                Pet pet2 = pets[petIndex2];
+
+                if ((pet1 is Cat && pet2 is Chimpanzee) || (pet1 is Chimpanzee && pet2 is Cat))
+                {
+                    Console.WriteLine("A cat and a chimpanzee met! They are friends!");
+
+                }
+                else if ((pet1 is Turtle && pet2 is Cat) || (pet1 is Cat && pet2 is Turtle))
+                {
+                    Console.WriteLine("A turtle and cat have met. It's a fight!");
+                }
+                else
+                {
+                    Console.WriteLine($"The pets {pet1.Name} and {pet2.Name} have met!");
+                }
+            }
             break;
         case "4":
             break;
